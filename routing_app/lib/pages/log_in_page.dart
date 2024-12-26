5b22f5c6f1c6e0938f07f8ebe32b71cb1629c21b
aditya <magaraditya2911@gmail.com>
2024-12-26 17:11:36 +0530

    start screen added

--- routing_app/lib/pages/log_in_page.dart
+++ routing_app/lib/pages/log_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:routing_app/widget/custom_button.dart';
 
 class LogInPage extends StatefulWidget {
   const LogInPage({super.key});
@@ -10,6 +11,38 @@ class LogInPage extends StatefulWidget {
 class _LogInPageState extends State<LogInPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Image.asset('assets/images/logo2.jpg'),
+
+          SizedBox(
+            child: Column(
+              children: [
+                CustomButton(color: Colors.white,
+                    textColor: Colors.black,
+                    label: "SignUp",
+                    onPressed: (){}),
+                const SizedBox(height: 15,),
+
+                CustomButton(color: Colors.black,
+                    textColor: Colors.white,
+                    label: "LogIn",
+                    onPressed: (){}),
+              ],
+            ),
+          ),
+
+          const Text("Continue as guest",
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 16,
+            fontWeight: FontWeight.bold
+          ),)
+        ],
+      ),
+    );
   }
 }
