9818adf6a0f71093a534b2d836c74f7b780994a9
Abhishek <abhisheksangule6@gmail.com>
2025-01-20 23:15:08 +0530

    add effect to AQI button

--- routing_app/lib/pages/profile_page.dart
+++ routing_app/lib/pages/profile_page.dart
@@ -1,13 +1,11 @@
 import 'dart:math';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:routing_app/pages/start_screen.dart';
-class ProfilePage extends StatefulWidget {
 
-  ProfilePage({super.key,
-  });
+class ProfilePage extends StatefulWidget {
+  ProfilePage({super.key});
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -54,170 +52,170 @@ class _ProfilePageState extends State<ProfilePage> {
   Color _generateRandomColor() {
     final Random random = Random();
     return Color.fromARGB(
-      255,                   // Fully opaque
-      random.nextInt(256),   // Red (0-255)
-      random.nextInt(256),   // Green (0-255)
-      random.nextInt(256),   // Blue (0-255)
+      255,
+      random.nextInt(256),
+      random.nextInt(256),
+      random.nextInt(256),
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Text(""),
         title: const Text('Profile'),
         centerTitle: true,
+        backgroundColor: Colors.teal,
         actions: [
           IconButton(
-            icon: const Icon(Icons.output_outlined),
+            icon: const Icon(Icons.logout),
             onPressed: () {
-              showDialog(context: context,
-                  builder: (context){
-                    return AlertDialog(
-                      title: const Text("Are you sure you want to sign out ?"),
-                      actions: [
-                        TextButton(onPressed: () async{
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text("Sign Out"),
+                    content: const Text("Are you sure you want to sign out?"),
+                    actions: [
+                      TextButton(
+                        onPressed: () async {
                           await FirebaseAuth.instance.signOut();
-                          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>const StartScreen()));
-
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => const StartScreen()));
                         },
-                            child: const Text("Yes",style: TextStyle(color: Colors.red),)),
-
-                        TextButton(onPressed: (){
-
+                        child: const Text("Yes",
+                            style: TextStyle(color: Colors.red)),
+                      ),
+                      TextButton(
+                        onPressed: () {
                           Navigator.of(context).pop();
                         },
-                            child: const Text("No",style: TextStyle(color: Colors.blue),))
-                      ],
-                    );
-                  });
+                        child: const Text("No",
+                            style: TextStyle(color: Colors.blue)),
+                      ),
+                    ],
+                  );
+                },
+              );
             },
           ),
         ],
       ),
-      backgroundColor: Colors.white, // Set background color to white
+      backgroundColor: Colors.grey[100],
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('userInfo').where("userid",
-        isEqualTo: FirebaseAuth.instance.currentUser!.uid).snapshots(),
-         builder: (context,snapshot) {
+        stream: FirebaseFirestore.instance
+            .collection('userInfo')
+            .where("userid", isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final userDoc = snapshot.data!.docs[0];
 
-           return snapshot.data == null ? Center(child: CircularProgressIndicator(),):
-             SingleChildScrollView(
-
-             child: Column(
-               crossAxisAlignment: CrossAxisAlignment.center,
-               children: [
-                 const SizedBox(height: 20),
-                 CircleAvatar(
-                   radius: 50,
-                   backgroundColor: _generateRandomColor(),
-                   child: Text(snapshot.data!.docs[0].data()['name'].toString().substring(0,1).toUpperCase(),
-                   style: const TextStyle(fontSize: 50,color: Colors.white),)
-                 ),
-                 const SizedBox(height: 10),
-                 Text(
-
-                     "${snapshot.data!.docs[0].data()['name']}",
-                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                 ),
-                 const SizedBox(height: 10),
-
-                 ProfileDetailCard(
-                   title: 'Name',
-                   value: '${snapshot.data!.docs[0].data()['name']}',
-                   onTap: () {
-                     _showEditDialog(
-                       context: context,
-                       title: 'Name',
-                       initialValue: '${snapshot.data!.docs[0].data()['name']}',
-                       onSave: (newValue) {
-                         setState(() {
-                            FirebaseFirestore.instance.collection('userInfo').
-                           doc(snapshot.data!.docs[0].id).update({
-                              'name':newValue
-                            });
-                         });
-                         ScaffoldMessenger.of(context).showSnackBar(
-                           SnackBar(content: Text("Name updated to $newValue")),
-                         );
-                       },
-                     );
-                   },
-                 ),
-                 ProfileDetailCard(
-                   title: 'Email',
-                   value: '${snapshot.data!.docs[0].data()['email']}',
-                   onTap: () {
-                     _showEditDialog(
-                       context: context,
-                       title: 'Email',
-                       initialValue: 'sid@growthx.com',
-                       onSave: (newValue) {
-                         // Handle email save
-                         ScaffoldMessenger.of(context).showSnackBar(
-                           SnackBar(
-                               content: Text("Email updated to $newValue")),
-                         );
-                       },
-                     );
-                   },
-                 ),
-                 ProfileDetailCard(
-                   title: 'Phone Number',
-                   value: '${snapshot.data!.docs[0].data()['phone']}',
-                   onTap: () {
-                     _showEditDialog(
-                       context: context,
-                       title: 'Phone Number',
-                       initialValue: '+91 6952346752',
-                       onSave: (newValue) {
-                         setState(() {
-                           FirebaseFirestore.instance.collection('userInfo').
-                           doc(snapshot.data!.docs[0].id).update({
-                             'phone':newValue
-                           });
-                         });
-                         ScaffoldMessenger.of(context).showSnackBar(
-                           SnackBar(
-                               content: Text(
-                                   "Phone number updated to $newValue")),
-                         );
-                       },
-                     );
-                   },
-                 ),
-                 ProfileAboutSection(
-                   aboutText:
-                   '${snapshot.data!.docs[0].data()['about']}',
-                   onTap: () {
-                     _showEditDialog(
-                       context: context,
-                       title: 'About',
-                       initialValue:
-                       '${snapshot.data!.docs[0].data()['about']}',
-                       onSave: (newValue) {
-                         setState(() {
-                           FirebaseFirestore.instance.collection('userInfo').
-                           doc(snapshot.data!.docs[0].id).update({
-                             'about':newValue
-                           });
-                         });
-                         ScaffoldMessenger.of(context).showSnackBar(
-                           SnackBar(
-                               content: Text("About updated to $newValue")),
-                         );
-                       },
-                     );
-                   },
-                 ),
-               ],
-             ),
-           );
-        }
-
-         )
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+                CircleAvatar(
+                  radius: 60,
+                  backgroundColor: _generateRandomColor(),
+                  child: Text(
+                    userDoc['name'].toString().substring(0, 1).toUpperCase(),
+                    style: const TextStyle(fontSize: 40, color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  userDoc['name'],
+                  style: const TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal),
+                ),
+                const SizedBox(height: 20),
+                ProfileDetailCard(
+                  title: 'Name',
+                  value: userDoc['name'],
+                  onTap: () {
+                    _showEditDialog(
+                      context: context,
+                      title: 'Name',
+                      initialValue: userDoc['name'],
+                      onSave: (newValue) {
+                        FirebaseFirestore.instance
+                            .collection('userInfo')
+                            .doc(userDoc.id)
+                            .update({'name': newValue});
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Name updated to $newValue")));
+                      },
+                    );
+                  },
+                ),
+                ProfileDetailCard(
+                  title: 'Email',
+                  value: userDoc['email'],
+                  onTap: () {
+                    _showEditDialog(
+                      context: context,
+                      title: 'Email',
+                      initialValue: userDoc['email'],
+                      onSave: (newValue) {
+                        FirebaseFirestore.instance
+                            .collection('userInfo')
+                            .doc(userDoc.id)
+                            .update({'email': newValue});
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Email updated to $newValue")));
+                      },
+                    );
+                  },
+                ),
+                ProfileDetailCard(
+                  title: 'Phone Number',
+                  value: userDoc['phone'],
+                  onTap: () {
+                    _showEditDialog(
+                      context: context,
+                      title: 'Phone Number',
+                      initialValue: userDoc['phone'],
+                      onSave: (newValue) {
+                        FirebaseFirestore.instance
+                            .collection('userInfo')
+                            .doc(userDoc.id)
+                            .update({'phone': newValue});
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Phone updated to $newValue")));
+                      },
+                    );
+                  },
+                ),
+                ProfileAboutSection(
+                  aboutText: userDoc['about'],
+                  onTap: () {
+                    _showEditDialog(
+                      context: context,
+                      title: 'About',
+                      initialValue: userDoc['about'],
+                      onSave: (newValue) {
+                        FirebaseFirestore.instance
+                            .collection('userInfo')
+                            .doc(userDoc.id)
+                            .update({'about': newValue});
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("About updated to $newValue")));
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
@@ -238,11 +236,15 @@ class ProfileDetailCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      elevation: 3,
       child: ListTile(
-        title: Text(title),
+        title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
         subtitle: Text(value),
         trailing: IconButton(
-          icon: const Icon(Icons.edit),
+          icon: const Icon(Icons.edit, color: Colors.teal),
           onPressed: onTap,
         ),
       ),
@@ -264,11 +266,16 @@ class ProfileAboutSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      elevation: 3,
       child: ListTile(
-        title: const Text('About'),
+        title:
+            const Text('About', style: TextStyle(fontWeight: FontWeight.bold)),
         subtitle: Text(aboutText),
         trailing: IconButton(
-          icon: const Icon(Icons.edit),
+          icon: const Icon(Icons.edit, color: Colors.teal),
           onPressed: onTap,
         ),
       ),
